47ed12e761821ad3f84fc6847c1f8917c07d943e
Douglas Márquez <douglas09marquez@gmail.com>
2023-12-05 00:34:03 -0400

    first commit

---
+++ components/dropdown.js
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import { LocSvg } from './svg/loc'
+import { TwitterSvg } from './svg/twitter'
+import { InstagramSvg } from './svg/ig'
+import { FacebookSvg } from './svg/fb'
+
+
+
+function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('Bello monte'); // Inicializa el estado
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option); // Establece la opción seleccionada
+    setIsOpen(false); // Cierro el dropdown al hacer clic en una opción
+  };
+
+
+  return (
+    <div className="relative flex text-left text-sm align-middle">
+      <button
+        onClick={toggleDropdown}
+        className="flex items-center justify-between w-40 px-2 text-sm bg-transparent rounded-md focus:outline-none text-white hover:text-black"
+      >
+        <LocSvg fill="white" />
+        {selectedOption}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={`hover:text-black h-5 w-5 transition-transform transform ${isOpen ? 'rotate-180' : ''
+            }`}
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 12a1 1 0 01-.707-.293l-4-4a1 1 0 111.414-1.414L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4A1 1 0 0110 12z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+
+
+
+      {isOpen && (
+        <div className="absolute mt-8 w-40 bg-white rounded-md shadow-lg">
+          <ul className="py-1">
+            <li>
+              <button
+                className="block w-full px-4 py-2 text-left hover:bg-gray-100 text-sm"
+                onClick={() => handleOptionClick('La Trinidad')}
+              >
+                La Trinidad
+              </button>
+            </li>
+            <li>
+              <button
+                className="block w-full px-4 py-2 text-left hover:bg-gray-100 text-sm"
+                onClick={() => handleOptionClick('Las Mercedes')}
+              >
+                Las Mercedes
+              </button>
+            </li>
+            <li>
+              <button
+                className="block w-full px-4 py-2 text-left hover:bg-gray-100 text-sm"
+                onClick={() => handleOptionClick('Sebucan')}
+              >
+                Sebucan
+              </button>
+            </li>
+            <li>
+              <button
+                className="block w-full px-4 py-2 text-left hover:bg-gray-100 text-sm"
+                onClick={() => handleOptionClick('Bello monte')}
+              >
+                Bello Monte
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,151 @@
 import React from 'react';
-import Image from 'next/image';
-import logo from '../public/logo.png';
+import { useEffect, useRef, useState } from 'react'
+import { ShopSvg } from '../components/svg/shop'
+import { UserSvg } from '../components/svg/user'
+import { HouseSvg } from '../components/svg/house'
+import { LocSvg } from '../components/svg/loc'
+import { FacebookSvg } from '../components/svg/fb'
+import { InstagramSvg } from '../components/svg/ig'
+import { TwitterSvg } from '../components/svg/twitter'
+import DropDown from '../components/dropdown'
+import Slide from '../components/slideshow'
+
 
 function HomePage() {
+  const [state, setState] = useState(false)
+  const navRef = useRef<HTMLDivElement>(null);
+
+  const navigation = [
+    { title: "Inicio", path: "javascript:void(0)" },
+    { title: "Comprar por marca", path: "javascript:void(0)" },
+    { title: "Comprar por mascota", path: "javascript:void(0)" },
+    { title: "Nuestros servicios", path: "javascript:void(0)" },
+    { title: "Promociones", path: "javascript:void(0)" },
+    { title: "Nosotros", path: "javascript:void(0)" }
+  ]
+  useEffect(() => {
+    if (navRef.current) {
+      const body = document.body;
+      const customBodyStyle = ["overflow-hidden", "lg:overflow-visible"];
+      if (state) body.classList.add(...customBodyStyle);
+      else body.classList.remove(...customBodyStyle);
+      const customStyle = ["sticky-nav", "fixed", "border-b"];
+      window.onscroll = () => {
+        if (window.scrollY > 80 && navRef.current) {
+          navRef.current.classList.add(...customStyle);
+        } else if (navRef.current) {
+          navRef.current.classList.remove(...customStyle);
+        }
+      };
+    }
+  }, [state]);
+
   return (
-    <div className="min-h-screen w-screen bg-white flex dark:bg-black">
-      <section className="max-w-screen-md m-auto h-full">
-        <article className="">
-          <a href="https://avilatek.dev" target="_blank" rel="noreferrer">
-            <Image
-              className="w-96 h-auto text-center mx-auto"
-              alt="Avila Tek Logo"
-              src={logo}
-            />
-          </a>
-          <h1 className="text-2xl text-center text-gray-700 dark:text-gray-100">
-            Next.js Started template
-          </h1>
-          <p className="text-center text-white text-lg mt-2">
-            <a
-              href="https://github.com/Avila-Tek/next-template"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-          </p>
-        </article>
-      </section>
+    <div>
+      <div className="bg-primary-300 w-full h-[30px] relative z-40 flex justify-end">
+        <div className='flex align-middle relative z-50' >
+          <DropDown />
+          <div className=' flex items-center space-x-2 mr-8'>
+            <TwitterSvg />
+            <FacebookSvg />
+            <InstagramSvg />
+          </div>
+        </div>
+      </div>
+      <div className='flex items-center justify-between mx-auto px-4'>
+        <a className='ml-4' href="javascript:void(0)">
+          <img
+            src="/logomaxi.png"
+            width={169.46}
+            height={46}
+            alt="Maxipet"
+          />
+        </a>
+        <div className='flex items-center justify-center flex-1 '>
+          <form className="flex items-center space-x-2 p-2 max-w-3xl ">
+            <div className="relative flex w-full py-2">
+              <input
+                className="w-[320px] h-[33px] py-2 rounded-lg border border-gray-400 placeholder-gray-300 text-sm focus:outline-none "
+                type="text"
+                placeholder="Buscador"
+              />
+              <button
+                type="submit"
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-transparent border-none cursor-pointer "
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-300 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className='flex items-center space-x-3 ml-auto mr-4'>
+          <LocSvg fill="black" />
+          <HouseSvg />
+          <UserSvg />
+          <ShopSvg />
+        </div>
+      </div>
+
+      <nav ref={navRef} className="bg-white drop-shadow-lg relative  z-20  w-full top-0 items-center justify-center">
+        <div className="items-center  max-w-screen-xl mx-auto md:px-8 lg:flex">
+          <div className="flex items-center justify-between py-3 lg:py-4 lg:block">
+
+            <div className="lg:hidden">
+              <button className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
+                onClick={() => setState(!state)}
+              >
+                {
+                  state ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                    </svg>
+                  ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8h16M4 16h16" />
+                    </svg>
+                  )
+                }
+              </button>
+            </div>
+          </div>
+          <div className="items-center content-center justify-center mx-auto">
+            <ul className="justify-center items-center space-y-8 lg:flex gap-24 lg:space-y-0 ">
+              {
+                navigation.map((item, idx) => {
+                  return (
+                    <li key={idx} className="text-gray-600 hover:text-primary-300 sm:text-sm md:text-sm lg:text-sm xl:text-md">
+                      <a href={item.path}>
+                        {item.title}
+                      </a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Slide />
+
+
+
     </div>
+
+
   );
 }
 
